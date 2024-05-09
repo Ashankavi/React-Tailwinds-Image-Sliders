774856e1d6f19e4dfb753dafd3ce6be71749a42e
ashan <uhashan17929@gmail.com>
2024-05-09 12:31:39 +0530

    Add Slider03

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Slider01 from "./components/Slider01";
 import Slider02 from "./components/Slider02";
+import Slider03 from "./components/Slider03";
 
 function App() {
   return (
     <div className="App">
         <Slider01/>
         <Slider02/>
+        <Slider03/>
     </div>
   );
 }
--- src/components/Slider02.jsx
+++ src/components/Slider02.jsx
@@ -44,6 +44,15 @@ const Slider02 = () => {
   return (
 
     <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+
+
+                {/* 
+
+                Meeke images serm import krgnne google eken
+
+                */}
+
+
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
